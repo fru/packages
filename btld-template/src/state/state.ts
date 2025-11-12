50ed481bd8b8c3e303a2434a2af21105ba6a93de
Florian Rüberg <florian.rueberg@gmail.com>
2025-11-12 19:09:22 +0100

    .

--- btld-template/src/state/state.ts
+++ btld-template/src/state/state.ts
@@ -56,16 +56,71 @@ class State<T> {
         if (child !== $deleted) obj[key] = child;
       }
       return obj;
-    }
+    } 
     return data;
   }
 
   listen(path: NormalizedPath, listener: Listener, deep: boolean): void {
     const stored = this._store.get(path)!;
     if (deep) {
-      stored.listenerDeep.add(listener);
+      (stored.listenerDeep ??= new Set()).add(listener);
     } else {
-      stored.listenerThis.add(listener);
+      (stored.listenerThis ??= new Set()).add(listener);
     }
+  }
+  proxy(path: NormalizedPath = '' as NormalizedPath): unknown {
+    const { data, keys } = this._store.get(path)!;
+    if (data === $deleted) return undefined;
+    if (data === $array) {
+      const length = this.proxy(join(path, 'length')) as number;
+      return new Proxy(new Array(length), {
+        get: (target: unknown[], p: string | symbol): unknown => {
+          if (typeof p === 'symbol' || isNaN(Number(p))) {
+            return Reflect.get(target, p);
+          }
+          return this.proxy(join(path, Number(p)));
+        },
+        ownKeys: (target: unknown[]): ArrayLike<string> => {
+          return new Array(length).fill(0).map((_, i) => String(i));
+        },
+        getOwnPropertyDescriptor: (target: unknown[], p: string | symbol): PropertyDescriptor | undefined => {
+          if (typeof p === 'symbol' || isNaN(Number(p)) || Number(p) >= length) {
+            return undefined;
+          }
+          return {
+            enumerable: true,
+            configurable: true,
+            value: this.proxy(join(path, Number(p)))
+          };
+        }
+      });
+    }
+    if (data === $object) {
+      const obj: Record<string, unknown> = {};
+      return new Proxy(obj, {
+        get: (target: object, p: string | symbol): unknown => {
+          if (typeof p === 'symbol') {
+            return Reflect.get(target, p);
+          }
+          const subpath = keys.get(p as string);
+          if (!subpath) return undefined;
+          return this.proxy(subpath);
+        },
+        ownKeys: (): ArrayLike<string> => {
+          return Array.from(keys.keys());
+        },
+        getOwnPropertyDescriptor: (target: object, p: string | symbol): PropertyDescriptor | undefined => {
+          if (typeof p === 'symbol' || !keys.has(p as string)) {
+            return undefined;
+          }
+          return {
+            enumerable: true,
+            configurable: true,
+            value: this.proxy(keys.get(p as string)!)
+          };
+        }
+      });
+    }
+    return data;
   }
 }
