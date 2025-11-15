23308c4da3c20d4ef55a87152d6774ff3bac79e1
Florian Rüberg <florian.rueberg@gmail.com>
2025-11-15 11:36:32 +0100

    Fix pprettier

--- btld-template/src/tdd/state-recorder.spec.ts
+++ btld-template/src/tdd/state-recorder.spec.ts
@@ -5,34 +5,24 @@ function wrap<T>(data: T) {
 }
 
 bench('sort', () => {
-console.log('sorting');
-expect(true).toBe(true);
+  console.log('sorting');
+  expect(true).toBe(true);
 });
 
 it('Template should render and update on data change', () => {
-
   var { proxy, recorded } = wrap({
     test: {
       test2: {
-        array: [
-          { i: 2 },
-          { i: 1 },
-          { i: 0 },
-        ]
-      }
-    }
+        array: [{ i: 2 }, { i: 1 }, { i: 0 }],
+      },
+    },
   });
 
-  
-    
-    proxy.test.test2.array.sort((a, b) => a.i - b.i);
-  var i1 = proxy.test.test2.array.find(a => a.i === 1);
+  proxy.test.test2.array.sort((a, b) => a.i - b.i);
+  var i1 = proxy.test.test2.array.find((a) => a.i === 1);
   (<any>i1).other = true;
   (<any>proxy.test).test3 = undefined;
   (<any>proxy.test).test3 = { test4: true };
-
-
-
 
   expect(true).toBe(true);
 });
