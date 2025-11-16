ba6adfdef93f84740ed9eb14163162bb6d2f1016
Florian Rüberg <florian.rueberg@gmail.com>
2025-11-16 15:20:12 +0100

    .

--- btld-template/src/tdd/1-proxy-path.spec.ts
+++ btld-template/src/tdd/1-proxy-path.spec.ts
@@ -4,32 +4,37 @@ it('Template should render and update on data change', () => {
   type Test = {
     test2?: {
       array: {
+        test: { i: number };
         [key: string]: { i: number };
       }[];
     };
     test3?:
       | {
           test4?: boolean;
-        }
+        }[]
       | number;
+    test4: () => void;
+    test5: Date;
   };
 
   const proxy = createProxy<Test>();
   const x = proxy.test2.array[1].test.i;
   console.log(proxy.test2.array[-1.5].test.i[$path]);
+  proxy.test3[1].test4;
 });
 
 type StateProxyPath = { [$path]: string[] };
 const $path = Symbol('path');
 
 type Primitive = string | number | boolean | bigint | null | undefined;
-type ValueType = Primitive | Function;
+type Value = Primitive | Function;
 
-type Keyed<T> = T extends Array<infer U> ? { [index: number]: U } : Exclude<T, ValueType>;
-type Key<T> = Extract<keyof Keyed<T>, string | number>;
+type KeyedArray<T> = T extends Array<infer U> ? { [index: number]: U } : T;
+type KeyedObject<T> = T extends Record<string | number, unknown> ? T : {};
+type Keyed<T> = KeyedObject<KeyedArray<Exclude<T, Value>>>;
 
 export type StateProxy<T> = StateProxyPath & {
-  [K in Key<T>]: StateProxy<Keyed<T>[K]>;
+  [K in Extract<keyof Keyed<T>, string | number>]: StateProxy<Keyed<T>[K]>;
 };
 
 export function createProxy<T>(path: string[] = []): StateProxy<T> {
